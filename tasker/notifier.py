465411e4c210733c4befe8166d33ba2611bc7722
zigai <ziga.ivansek@gmail.com>
2023-04-01 01:02:01 +0200

    markdown formatter for notifiers

--- tasker/notifier.py
+++ tasker/notifier.py
@@ -1,36 +1,102 @@
-import os
-import platform
-import shlex
-import uuid
+import sys
 from dataclasses import asdict, dataclass, field
-from pathlib import Path
-from socket import gethostname
 
-from apprise import Apprise
+from apprise import Apprise, NotifyFormat
 from loguru import logger as log
-from stdl import fs
+from stdl.dt import fmt_datetime
+from stdl.log import loguru_formater
+
+log.remove()
+log.add(sys.stdout, level="DEBUG", format=loguru_formater)
+
+
+class Event:
+    START = "start"
+    SUCCESS = "success"
+    FAIL = "fail"
+    INFO = "info"
 
 
 @dataclass(frozen=True)
 class Channel:
-    channel: str
-    events: list[str] = field(default_factory=lambda: ["info"])
+    url: str
+    events: list[str] = field(default_factory=lambda: [Event.INFO, Event.FAIL])
+
+
+class MarkdownNotificationFormatter:
+    format = NotifyFormat.MARKDOWN  # type: ignore
+    min_ljust = 10
+    max_ljust = 20
+
+    event_titles = {
+        Event.START: "STARTING",
+        Event.SUCCESS: "SUCCESS",
+        Event.FAIL: "FAIL",
+        Event.INFO: "INFO",
+    }
+
+    event_emojis = {
+        Event.START: ":blue_circle:",
+        Event.SUCCESS: ":green_circle:",
+        Event.FAIL: ":red_circle:",
+        Event.INFO: ":white_circle:",
+    }
+
+    def __init__(self, ms=True) -> None:
+        self.ms = ms
+
+    def _get_title(self, name: str, event: str) -> str:
+        title = f"{self.event_emojis[event]} `{fmt_datetime(d_sep='/',ms=self.ms)}`  **|**  name:  `{name}`"
+        return title
+
+    def format_notification(self, name: str, event: str, data: dict, sections: dict) -> str:
+        title = self._get_title(name, event)
+        if data:
+            body = ">>> "
+            for key, value in data.items():
+                body += f"{key}: `{value}`\n"
+        else:
+            body = ""
+        if sections:
+            body += "\n"
+            for key, value in sections.items():
+                body += f"**{key}**\n```{value}```\n"
+        return title + " \n" + body
 
 
 class Notifier:
-    EVENTS = ["start", "success", "error", "result", "info"]
+    EVENTS = ["start", "success", "fail", "info"]
 
-    def __init__(self) -> None:
+    def __init__(self, name: str, fmt_class=MarkdownNotificationFormatter) -> None:
+        self.name = name
+        self.formatter = fmt_class()
         self.notifiers: dict[str, Apprise] = {}
-        for name in self.EVENTS:
-            self.notifiers[name] = Apprise()
+        for event in self.EVENTS:
+            self.notifiers[event] = Apprise()
 
-    def subscribe(self, notification: Channel):
-        for name in notification.events:
-            self.notifiers[name].add(notification.channel)
+    def subscribe(self, channel: Channel):
+        for event in channel.events:
+            self._validate_event(event)
+            self.notifiers[event].add(channel.url)
 
-    def notify(self, event: str, body: str, title: str = ""):
+    def send_notification(self, event: str, data: dict, sections: dict):
+        self._validate_event(event)
+        body = self.formatter.format_notification(
+            name=self.name, event=event, data=data, sections=sections
+        )
+        self.notifiers[event].notify(body=body, body_format=self.formatter.format)
+        log.info(f"[{event}] {self.name}", **data)
+
+    def _validate_event(self, event: str):
         if event not in self.EVENTS:
-            log.warning(f"Event '{event}' is not a valid event. Valid events are: {self.EVENTS}")
-            return
-        self.notifiers[event].notify(body=body, title=title)
+            raise ValueError(
+                f"Event '{event}' is not a valid event. Valid events are: {', '.join(self.EVENTS)}"
+            )
+
+
+__all__ = [
+    "Event",
+    "Channel",
+    "MarkdownNotificationFormatter",
+    "Notifier",
+]
